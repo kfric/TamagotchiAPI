efa6ddf4e4ce0594faa6f48bdca387829c186465
Karl Frick <karl.frick5@gmail.com>
2021-05-06 13:44:40 -0400

    PetsController added

---
+++ Models/Pet.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TamagotchiAPI.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public DateTime Birthday { get; set; }
+        public int HungerLevel { get; set; }
+        public int HappinessLevel { get; set; }
+    }
+}
---
+++ Models/Scolding.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TamagotchiAPI.Models
+{
+    public class Scolding
+    {
+        public int Id { get; set; }
+        public DateTime When { get; set; }
+        public int PetId { get; set; }
+        public Pet Pet { get; set; }
+
+    }
+}
